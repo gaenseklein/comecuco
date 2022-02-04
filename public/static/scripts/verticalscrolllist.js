bfd3648a499d52d080f60a49678f0663833358f0
jonny <dekan@gaenseklein.net>
2022-02-04 05:22:39 +0100

    vertical scroll tamano de elementos

--- public/static/scripts/verticalscrolllist.js
+++ public/static/scripts/verticalscrolllist.js
@@ -7,6 +7,7 @@ function scrollList(button){
   let listwrapper = list.parentElement;
   let actscroll = listwrapper.scrollLeft;
   let scrollamount = Math.floor(list.clientWidth/listelements.length)
+  scrollamount = listelements[0].clientWidth+20;
   if(toRight){
     let newscroll=actscroll + scrollamount;
     if(newscroll>listwrapper.scrollLeftMax){
@@ -24,3 +25,20 @@ function scrollList(button){
     buttons[1].disabled=false;
   }
 }
+
+function scrollListInit(wrapperId){
+  let wrapper = document.getElementById(wrapperId);
+  let targetspace = wrapper.querySelector('.vertical-scroll-wrapper');
+  let listelements = targetspace.querySelectorAll('li');
+  let sizebetween = 20;
+  let maxItems = 3;
+  if(maxItems>listelements.length)maxItems=listelements.length;
+  let elementwidth= targetspace.offsetWidth / maxItems- sizebetween;
+  for (let x=0;x<listelements.length;x++){
+    listelements[x].style.width=elementwidth+"px";
+    listelements[x].style.marginright=sizebetween+"px";
+  }
+  console.log('elwidth:',elementwidth,'maxspace:',targetspace.offsetWidth);
+}
+
+scrollListInit('cajacolumna');
