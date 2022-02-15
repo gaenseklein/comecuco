8e6ea18348bf012bbb96d021e297bdc7278e4e0d
javierllugany <ojosdelplata@yahoo.com.ar>
2022-02-16 00:57:28 +0100

    Subpagina Usuario y EDITNoticia

--- public/static/scripts/verpassword2.js
+++ public/static/scripts/verpassword2.js
@@ -7,3 +7,14 @@ verpassword.addEventListener("mouseup", ( ) => {
               // Agregamos type de input
               password.setAttribute("type", "password");
 });
+
+
+// primero seleccionamos el elemento id del button
+verpassword2.addEventListener("mousedown", ( ) =>{
+              // Eliminamos su type del input
+               repeatpassword.removeAttribute("type");
+});
+verpassword2.addEventListener("mouseup", ( ) => {
+              // Agregamos type de input
+              repeatpassword.setAttribute("type", "password");
+});
