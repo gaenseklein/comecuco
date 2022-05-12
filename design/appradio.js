f01d646bb5ba3ec020303986e3145d4057683501
gaenseklein <dekan@gaenseklein.net>
2022-05-12 23:28:14 +0200

    nueva radio app

---
+++ design/appradio.js
@@ -0,0 +1,99 @@
+var radioapp={
+  redes:[ //contiene todas redes, cada red es un objeto
+    {
+      nombre: 'comecuco',
+      //si miembro tiene un mountpoint se toma base + mountpoint para src del audio
+      //si no usa el src directo del miembro:
+      base: 'https://comecuco.org:9000/', 
+      miembros: [
+        {tipo: 'radio', nombre: 'la lenera', mountpoint:"lalenera"},
+        {tipo: 'radio', nombre: 'radio cuyum', mountpoint:"Cuyum"},
+        {tipo: 'radio', nombre: 'radio sin dueno', mountpoint:"RadiosinDueno"},
+        {tipo: 'radio', nombre: 'tierra campesina', mountpoint:"TierraCampesina"},
+        {tipo: 'video', nombre: 'giramundo tv', youtube:true, src:'https://www.youtube.com/embed/i4rlKGYUPTo'},
+      ],
+    }, //fin red comecuco
+    {
+      nombre: 'farco',
+      miembros:[
+        {tipo: 'radio', nombre:'farcoradio', src:'ALasBarricadas.mp3'},
+        {tipo: 'radio', nombre:'farcoradio 2', src:'ALasBarricadas.mp3'},
+      ],
+    },
+    {
+      nombre: 'rnma',
+      miembros:[
+        {tipo: 'radio', nombre:'rnma-radio 1', src:'radio.farco.org'},
+        {tipo: 'radio', nombre:'rnma-radio 2', src:'radio.farco.org'},
+      ],
+    },
+    {
+      nombre: 'amarc',
+      miembros:[
+        {tipo: 'radio', nombre:'amarcradio', src:'radio.farco.org'},
+      ],
+    },
+    {
+      nombre: 'radios libres',
+      miembros:[
+        {tipo: 'radio', nombre:'radios libres', src:'radio.farco.org'},
+        {tipo: 'radio', nombre:'radios libres 2', src:'radio.farco.org'},
+        {tipo: 'radio', nombre:'radios libres 3', src:'radio.farco.org'},
+      ],
+
+    },
+  ], //fin lista redes
+  redActual: {},
+  radioActual: 0,
+  radioplayer:null,
+  init: function(){
+    this.redActual=this.redes[0]
+    this.radioplayer=document.getElementById('radioplayer')
+    this.cambiaRadioAId(0)
+  },
+  cambiaRadio: function(izquierda){
+    if(izquierda)this.radioActual--;
+    else this.radioActual++;
+    if(this.radioActual>=this.redActual.miembros.length)this.radioActual=0
+    if(this.radioActual<0)this.radioActual=this.redActual.miembros.length-1
+    this.cambiaRadioAId(this.radioActual)
+  },
+  cambiaRadioAId: async function(index){
+    let miembroact = this.redActual.miembros[index]
+    this.radioActual = index
+    if(miembroact.tipo=='radio'){
+      let src=miembroact.src
+      if(miembroact.mountpoint)src=this.redActual.base + miembroact.mountpoint
+      this.radioplayer.src=src
+      this.radioplayer.load()
+    }else{
+      this.radioplayer.pause()
+    }
+
+    COMECUCOactivo.innerHTML=`<h5>${miembroact.nombre}</h5>`
+  },
+  cambiaRed: function(){
+    if(Banda.checked)return;
+    let radiobs = document.querySelectorAll('input[name=redes]');
+    let radioid;
+    for (let x=0;x<radiobs.length;x++){
+      if(radiobs[x].checked){
+        radioid=radiobs[x].id
+        break;
+      }
+    }
+    if(!radioid)return;
+    radioid=radioid.toLowerCase()
+    for (x=0;x<this.redes.length;x++){
+      if(this.redes[x].nombre==radioid){
+        this.redActual=this.redes[x]
+        break
+      }
+    }
+    this.cambiaRadioAId(0)
+  },
+  cambiaVolumen: function(volumen){
+    this.radioplayer.volume=volumen;
+  },
+}
+radioapp.init();
