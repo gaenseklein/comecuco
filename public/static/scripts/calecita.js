85ebca50f767220cfc6b7ca6dcd8f07c495b6977
javierllugany <ojosdelplata@yahoo.com.ar>
2022-09-20 19:28:28 +0200

    agrego controles manuales a calesita de frontpage y en subpag de editnoticia aumento la cantidad de caracteres para el titulo

--- public/static/scripts/calecita.js
+++ public/static/scripts/calecita.js
@@ -11,3 +11,26 @@
     calecitatimer=setTimeout(calecita, calecitatiempo);
   }
   calecita();
+
+
+  function calecitanextya(){
+    let lis = document.querySelectorAll('.calecita li');
+    let oldli=document.querySelector('.calecita li.activo');
+    let valorlisNext=0+calecitaactivo;
+    if(valorlisNext>3)valorlisNext=0;
+    if(oldli)oldli.classList.remove('activo');
+    if(lis[valorlisNext])lis[valorlisNext].classList.add('activo');
+    calecitaactivo=valorlisNext++;
+    if(calecitaactivo>3)calecitaactivo=0;
+  }
+
+  function calecitaprevya(){
+    let lis = document.querySelectorAll('.calecita li');
+    let oldli=document.querySelector('.calecita li.activo');
+    let valorlisPrev=0+calecitaactivo-1;
+    if(valorlisPrev<0)valorlisPrev=3;
+    if(oldli)oldli.classList.remove('activo');
+    if(lis[valorlisPrev])lis[valorlisPrev].classList.add('activo');
+    calecitaactivo=valorlisPrev--;
+    if(calecitaactivo<0)calecitaactivo=3;
+  }
