66ea571f8bc7ccc5bff213aadafcabec71553a3f
gaenseklein <dekan@gaenseklein.net>
2022-10-11 00:24:37 +0200

    orden y menor cantidad de redes sociales

--- public/static/scripts/rssharelink.js
+++ public/static/scripts/rssharelink.js
@@ -3,14 +3,14 @@ function crearLinksRedesSociales(){
     let title = document.querySelector('.titulopagina').innerText
     let redes = {
        telegram : 'https://t.me/share/url?url='+encodeURIComponent(urlraw),
-        threema : 'threema://compose?text='+encodeURIComponent(title+' - '+urlraw),
-        whatsapp : 'whatsapp://send?text='+encodeURIComponent(title+' - '+urlraw),
-        diaspora : 'https://share.diasporafoundation.org/?url='+encodeURIComponent(urlraw)+'&title='+encodeURIComponent(title),
-        twitter: 'https://twitter.com/intent/tweet?text='+encodeURIComponent(title)+'&url='+encodeURIComponent(urlraw),
-        facebook : 'https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(urlraw),
-        reddit : 'https://reddit.com/submit?url='+encodeURIComponent(urlraw)+'&title='+encodeURIComponent(title),
-        tumblr : 'http://tumblr.com/widgets/share/tool?canonicalUrl='+encodeURIComponent(urlraw),
-        mail: 'mailto:foo@example.com?subject='+encodeURIComponent('te comparto la noticia de comecuco: '+title)+'&body='+encodeURIComponent(urlraw),
+       whatsapp : 'whatsapp://send?text='+encodeURIComponent(title+' - '+urlraw),
+       twitter: 'https://twitter.com/intent/tweet?text='+encodeURIComponent(title)+'&url='+encodeURIComponent(urlraw),
+       diaspora : 'https://share.diasporafoundation.org/?url='+encodeURIComponent(urlraw)+'&title='+encodeURIComponent(title),
+       facebook : 'https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(urlraw),
+       mail: 'mailto:foo@example.com?subject='+encodeURIComponent('te comparto la noticia de comecuco: '+title)+'&body='+encodeURIComponent(urlraw),
+        // threema : 'threema://compose?text='+encodeURIComponent(title+' - '+urlraw),
+        // reddit : 'https://reddit.com/submit?url='+encodeURIComponent(urlraw)+'&title='+encodeURIComponent(title),
+        // tumblr : 'http://tumblr.com/widgets/share/tool?canonicalUrl='+encodeURIComponent(urlraw),
     }
     let keys = Object.keys(redes)
     let links = []
