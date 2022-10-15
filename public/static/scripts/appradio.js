1e89fd401c0d258974c10119869efe4b6c5da25a
javierllugany <ojosdelplata@yahoo.com.ar>
2022-10-15 07:49:44 +0200

    arreglo funcion textoscroll() en appradio.js y actualizado en script/appradio.js

--- public/static/scripts/appradio.js
+++ public/static/scripts/appradio.js
@@ -91,8 +91,8 @@ var radioapp={
     displayPause.style.transform= "rotate(-90deg)";
   },
   cambiaRadio: function(izquierda){
-    document.getElementById('m1Text').classList.remove('enMovimiento1');
-    document.getElementById('m2Text').classList.remove('enMovimiento2');
+    document.getElementById('m1Text').classList.remove('enMovimiento');
+    document.getElementById('m2Text').classList.remove('enMovimiento');
     if(izquierda)this.radioActual--;
     else this.radioActual++;
     if(this.radioActual>=this.redActual.miembros.length)this.radioActual=0
@@ -187,18 +187,18 @@ var radioapp={
     if(mem2){
       this.memoria2 = memoria
       M2.firstChild.innerText = memoria.radionombre
-      if (document.getElementById('m2Text').classList=="enMovimiento2"){
+      if (document.getElementById('m2Text').classList=="enMovimiento"){
         return
       }
-      this.textoScroll2();
+      this.textoScroll(true);
     }else{
       this.memoria1 = memoria
       console.log('saving',M1.firstChild);
       M1.firstChild.innerText = memoria.radionombre
-      if (document.getElementById('m1Text').classList=="enMovimiento1"){
+      if (document.getElementById('m1Text').classList=="enMovimiento"){
         return
       }
-      this.textoScroll()
+      this.textoScroll(false)
     }
   },
   playMemoria: function(mem2){
@@ -218,56 +218,42 @@ var radioapp={
     this.cambiaRadioAId(memoria.index)
   },
   espacioX: 0,
-  espacioX2: 0,
 
-  textoScroll: function(){
-    let textoEnM1=document.getElementById('m1Text');
-    if (textoEnM1.innerText.length<=14) {
-      textoEnM1.style.textIndent="0ch";
+  textoScroll: function(mov){
+    if (mov){var textoEnMem=document.getElementById('m2Text');
+  }else {var textoEnMem=document.getElementById('m1Text')
+    }
+    if (textoEnMem.innerText.length<=14) {
+      textoEnMem.style.textIndent="0ch";
       return
     }
-    let cantidadDeCaracteres=(textoEnM1.innerText.length-(textoEnM1.innerText.length*2))*1.5;
-    document.getElementById('m1Text').classList.add('enMovimiento1');
+    let cantidadDeCaracteres=(textoEnMem.innerText.length-(textoEnMem.innerText.length*2))*1.5;
+    textoEnMem.classList.add('enMovimiento');
+
     if(this.espacioX>cantidadDeCaracteres) {
       let f=this.espacioX+"ch"
-      textoEnM1.style.textIndent=f;
+      textoEnMem.style.textIndent=f;
     }
       if (this.espacioX<cantidadDeCaracteres){
         this.espacioX=+10;
       }
      else {this.espacioX=this.espacioX-0.5;
        }
-      this.textomovimiento();
-  },
-
-  textoScroll2: function(){
-    let textoEnM2=document.getElementById('m2Text');
-    if (textoEnM2.innerText.length<=14) {
-      textoEnM2.style.textIndent="0ch";
-      return
-    }
-   let cantidadDeCaracteres=(textoEnM2.innerText.length-(textoEnM2.innerText.length*2))*1.5;
-   document.getElementById('m2Text').classList.add('enMovimiento2');
-      if(this.espacioX2>cantidadDeCaracteres) {
-        let f=this.espacioX2+"ch"
-        textoEnM2.style.textIndent=f;
+      if (mov){
+      this.textomovimiento2();
+    }else {
+        this.textomovimiento();
       }
-        if (this.espacioX2<cantidadDeCaracteres){
-          this.espacioX2=+10;
-        }
-       else {this.espacioX2=this.espacioX2-0.5;
-         }
-        this.textomovimiento2();
   },
 
   textomovimiento: async function(){
       let espera= await this.sleep(150);
-      this.textoScroll()
+      this.textoScroll(false)
 },
 
   textomovimiento2: async function(){
       let espera= await this.sleep(150);
-      this.textoScroll2()
+      this.textoScroll(true)
 },
 
     sleep: function(ms) {
