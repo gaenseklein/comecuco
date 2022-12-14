f0741ec04dfd715d5d21444b4111ab6966a2a8cf
gaenseklein <dekan@gaenseklein.net>
2022-12-14 20:47:15 +0100

    nuevo reloj, mas corto aun, usando formatos predeclarados

--- public/static/scripts/horademendoza.js
+++ public/static/scripts/horademendoza.js
@@ -6,10 +6,24 @@ function horademendoza(){
 }
 
 function fechademendoza(){
+  let d = new Date()
+  let result = d.toLocaleDateString('es',{timeZone:'America/Buenos_Aires', dateStyle:'full'})
+  //borramos el comma? si no borre lo siguiente
+  let posDeComa = result.indexOf(',')
+  if(posDeComa>-1){
+    result = result.substring(0,posDeComa)+result.substring(posDeComa+1)
+  }
+  return result.toUpperCase()
+}
+
+function fechademendoza_viejo(){
+  //lo dejo aqui para aprender: asi podrias usar un "timeshift" - es decir nos tomamos tiempo de utc de ahora,
+  //borramos 4 horas y usamos como tiempo de mendoza
   let d = new Date();
   d.setTime(d.getTime() - 4 * 60000);
   let meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
   let dias = ['domingo','lunes', 'martes','miercoles','jueves','viernes','sabado','domingo']
+  //lo importante es usar UTCDay etc. porque no quieremos cambiar a la hora local del navegador:
   let dia = dias[d.getUTCDay()]
   let dianum = d.getUTCDate()
   let mes = meses[d.getUTCMonth()]
